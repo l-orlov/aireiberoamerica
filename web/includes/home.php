6e61734d89f842b96c0b51a21ad8965ab6735660
Lev Orlov <lev.orlov@dif.tech>
2026-03-09 21:56:55 -0300

    Fix base code icon

--- web/includes/home.php
+++ web/includes/home.php
@@ -2,11 +2,10 @@
   <div class="under_construction_block">
     <img src="img/logo.png" class="logo_img" alt="AIRE">
     <div class="construction_message">
-      <span class="construction_icon" aria-hidden="true">
-        <!-- hard hat / construction -->
+      <span class="construction_icon" aria-hidden="true" title="En construcción">
+        <!-- wrench = under construction / work in progress -->
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
-          <path d="M12 2c-2 0-4 1.5-4 4v2H4v9h16v-9h-4V6c0-2.5-2-4-4-4z"/>
-          <path d="M8 8h8"/>
+          <path d="M14.7 6.3a1 1 0 0 0 0 1.4l1.6 1.6a1 1 0 0 0 1.4 0l3.77-3.77a6 6 0 0 1-7.94 7.94l-6.91 6.91a2.12 2.12 0 0 1-3-3l6.91-6.91a6 6 0 0 1 7.94-7.94l-3.76 3.76z"/>
         </svg>
       </span>
       <span class="construction_text">Sitio en construcción</span>
